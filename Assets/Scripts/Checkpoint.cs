302748cee36ab59fce182496ba7ca70b303bb57c
Pang Zheng Yu <e0003788@nus.edu.sg>
2019-02-26 16:38:48 +0800

    Add SteamVR

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Checkpoint : MonoBehaviour
+{
+    private bool isCollided = false;
+    private float materialTransparency;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialTransparency = GetComponent<MeshRenderer>().material.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!isCollided) {
+            isCollided = true;
+            GetComponent<MeshRenderer>().material.DOFade(0.0f, 1.0f).OnComplete(() => {
+                GetComponent<MeshRenderer>().material.DOFade(materialTransparency, 1.0f).SetDelay(2.0f);
+                isCollided = false;
+            });
+        }
+    }
+   
+}
---
+++ Assets/Scripts/Stroke.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Stroke : MonoBehaviour
+{
+    private List<GameObject> checkpointList;
+
+    private void Awake() {
+        checkpointList = new List<GameObject>();
+
+        float transparency = 0.8f;
+        float increment = transparency / transform.transform.childCount;
+
+        for (int i = 0; i < transform.childCount; i++) {
+            checkpointList.Add(transform.GetChild(i).gameObject);
+            checkpointList[i].GetComponent<MeshRenderer>().material.DOFade(transparency, 0.1f);
+            transparency -= increment;
+        }
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
